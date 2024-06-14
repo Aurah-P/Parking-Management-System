bde78cb9d59f5d0ea9cb2b826f0f3a164adea25a
Aurah-P <167772589+Aurah-P@users.noreply.github.com>
2024-06-14 11:15:19 +0300

    Update ParkingSys.py

--- ParkingSys.py
+++ ParkingSys.py
@@ -2,20 +2,18 @@ from machine import Pin, ADC, PWM,SPI
 from ssd1306 import SSD1306_SPI
 import framebuf
 import time
-buz = Pin(11, Pin.OUT)
-servoE = PWM(Pin(14))
+buz = Pin(11, Pin.OUT) #This is the buzzer
+servoE = PWM(Pin(14)) #This is the servo motor at the entrance gate
 servoE.freq(50)
-servoX = PWM(Pin(15))
+servoX = PWM(Pin(15)) #This is the servo motor at the exit gate
 servoX.freq(50)
-led1 = Pin(1, Pin.OUT)
-sensE = Pin(13, Pin.IN, Pin.PULL_DOWN)
-sensX = Pin(12, Pin.IN, Pin.PULL_DOWN)
+led1 = Pin(1, Pin.OUT) #This pin was connected to both the laser module at the entrance and at the exit
+sensE = Pin(13, Pin.IN, Pin.PULL_DOWN) #This is the photoresistor at the entrance gate
+sensX = Pin(12, Pin.IN, Pin.PULL_DOWN) #This is the photoresistor at the exit gate
 spi = SPI(0, 100000, mosi=Pin(19), sck=Pin(18))
 oled = SSD1306_SPI(128, 64, spi, Pin(17),Pin(20), Pin(16))
-'''
-The terminals of the photoresistor have been interchanged due to an error in the soldering
-The piezzo takes a lot of time to drop the output, it needs a capacitor to draw the current quickly
-'''
+
+
 oled.fill(0)
 oled.show()
 oled.text("PARKING SYSTEM",0,0)
@@ -24,17 +22,15 @@ time.sleep(1.3)
 oled.text("Designed by Aura",0,20)
 oled.show()
 time.sleep(2)
-count = 10
+count = 10 #Total number of parking spaces available
 
 while True:
         buz.value(1)
         value = 4500
         led1.value(1)
-        #print(o)
         if sensE.value()==0 and sensX.value()==0:
                 servoE.duty_u16(7500)
                 servoX.duty_u16(value)
-                #print("Y")
                 time.sleep(0.5)
                 oled.fill(0)
                 oled.show()
@@ -46,7 +42,6 @@ while True:
         elif sensE.value() == 1 and sensX.value() == 1:
             servoE.duty_u16(value)
             servoX.duty_u16(7500)
-            #print("N")
             oled.fill(0)
             oled.show()
             oled.text("PARKING LOT",0,0)
@@ -60,7 +55,6 @@ while True:
             count = count + 1
             if count <=10 and count>0:
                 servoX.duty_u16(7500)
-                #print("J")
                 servoE.duty_u16(7500)
                 oled.fill(0)
                 oled.show()
@@ -88,7 +82,6 @@ while True:
                 buz.value(1)
                 time.sleep(1)
         elif sensE.value()==1 and sensX.value()==0:
-            #print("L")
             if count <=10 and count>0:
                 count = count - 1
                 servoE.duty_u16(value)
